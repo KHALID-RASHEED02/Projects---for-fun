3a377a4d2602cbf0f9f80f02c0801201e0e1e74a
KHALID-RASHEED02 <KHALID.ALB2002@GMAIL.COM>
2024-08-10 21:15:12 +0300

    Add files via upload

---
+++ Project1/Project1/Debug/Project1.tlog/خالد رشيد البلوي_ LAB1_Exercise2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c, f;
+	cout << "inter namer a";
+	cin >> a;
+	cout << "inter namer a";
+	cin >> b;
+	cout << "inter namer a";
+	cin >>c;
+	if (c<=0)
+
+	{
+		cout << "don't 0";
+
+	}
+	else
+	{
+		cout << "f=" << a + b ^ 2 / c;
+	}
+	return 0;
+
+
+
+
+}
---
+++ Project1/Project1/خالد رشيد البلوي _ LAB1_Exercise1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cout << "Enter a number";
+	cin >> a;
+	if ((a%2)==0)
+	{
+		cout << "is even";
+	}
+	else if ((a % 2) != 0)
+	{
+		cout << "is odd";
+
+	}
+	
+	return 0;
+
+	
+
+
+
+
+
+
+
+}
