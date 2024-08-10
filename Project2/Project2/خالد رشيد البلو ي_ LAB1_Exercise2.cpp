cbcf2085d35b0eb25dcb76e17de509e7cceaae7f
KHALID-RASHEED02 <KHALID.ALB2002@GMAIL.COM>
2024-08-10 21:19:28 +0300

    Add files via upload

---
+++ Project2/Project2/خالد رشيد البلو ي_ LAB1_Exercise2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cout << "inter nmber";
+	cin>> a;
+	cout << "inter nmber";
+	cin >> b;
+	cout << "inter nmber";
+	cin >> c;
+
+	if (c<=0)
+	{
+		cout << "inter nmber up 0";
+	}
+	else
+	{
+		cout <<"inter "<< pow(a + b, 2) / c;
+
+
+	}
+
+
+
+
+
+
+
+}
+
+
